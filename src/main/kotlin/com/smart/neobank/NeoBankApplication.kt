1d8ca4f79069943a120df60ec900d8d2d34716fb
aliJavaTar <ali77erfagh99@gmail.com>
2024-06-14 16:55:33 +0330

    first commit

---
+++ src/main/kotlin/com/smart/neobank/NeoBankApplication.kt
@@ -0,0 +1,11 @@
+package com.smart.neobank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeoBankApplication
+
+fun main() {
+	runApplication<NeoBankApplication>()
+}
