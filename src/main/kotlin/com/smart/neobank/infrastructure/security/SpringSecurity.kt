1d7e263b2e1e071c7c263c17459e2400a3507826
aliJavaTar <ali77erfagh99@gmail.com>
2024-06-14 17:47:37 +0330

    add security

---
+++ src/main/kotlin/com/smart/neobank/infrastructure/security/SpringSecurity.kt
@@ -0,0 +1,11 @@
+package com.smart.neobank.infrastructure.security
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+class SpringSecurity {
+
+//    fun configure(httpSecurity: HttpSecurity): SecurityFilterChain {
+//        httpSecurity.authorizeRequests()
+//    }
+}
