174d801b56580def314f6a4aa4d6d2647bb5c597
aliJavaTar <ali77erfagh99@gmail.com>
2024-06-15 14:36:53 +0330

    refactor

--- src/main/kotlin/com/smart/neobank/infrastructure/security/SpringSecurity.kt
+++ src/main/kotlin/com/smart/neobank/infrastructure/security/SpringSecurity.kt
@@ -3,7 +3,6 @@ package com.smart.neobank.infrastructure.security
 import com.smart.neobank.infrastructure.security.filter.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
